a23fe64da9ae8728fe1be4fa160b8d94c4d8580f
Jacksson Andrey <jaab.andrey@gmail.com>
2020-10-06 15:16:25 -0300

    add settings for send mail

--- avantz/settings.py
+++ avantz/settings.py
@@ -154,7 +154,18 @@ CORS_ALLOW_METHODS = [
     'PUT',
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Email Settings
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+# EMAIL_USE_SSL
+# EMAIL_TIMEOUT
+# EMAIL_SSL_KEYFILE
+# EMAIL_SSL_CERTFILE
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
