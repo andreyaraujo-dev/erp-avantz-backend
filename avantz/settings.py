5098cce7ed4f17cb1b3def317ba3328718d50219
Jacksson Andrey <jaab.andrey@gmail.com>
2020-11-23 19:43:23 -0300

    add hosts e cors permitido

--- avantz/settings.py
+++ avantz/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', 'https://gcomdev.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'https://gcomdev.herokuapp.com',
+                 'https://api-gcomdev.herokuapp.com']
 
 
 # Application definition
@@ -144,12 +145,14 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://localhost:8000',
     'https://gcomdev.herokuapp.com',
+    'https://api-gcomdev.herokuapp.com',
 ]
 # CORS URLS settings
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8000",
     'https://gcomdev.herokuapp.com',
+    'https://api-gcomdev.herokuapp.com',
 ]
 # List of HTTP verbs
 CORS_ALLOW_METHODS = [
@@ -171,6 +174,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:8000',
     'https://gcomdev.herokuapp.com',
+    'https://api-gcomdev.herokuapp.com',
     # other allowed origins...
 ]
 
