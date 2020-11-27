e24dd46fda16f664a4bbad36c8b086f2e9177601
Jacksson Andrey <jaab.andrey@gmail.com>
2020-11-27 17:30:52 -0300

    crud completed for the person mails

--- emails/urls.py
+++ emails/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import index, store
+from .views import index, store, update, delete
 
 urlpatterns = [
     path('<int:id_person>', index, name='get_all_mails'),
     path('create', store, name='register_mail_user'),
+    path('update', update, name='update_mail_user'),
+    path('delete/<int:id_mail>', delete, name='delte_mail_user'),
 ]
--- emails/views.py
+++ emails/views.py
@@ -4,6 +4,7 @@ from rest_framework import exceptions
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework import status
+import json
 
 from users.authentication import SafeJWTAuthentication
 from instituicao.models import Instit
@@ -30,10 +31,46 @@ def index(request, id_person):
 @authentication_classes([SafeJWTAuthentication])
 @ensure_csrf_cookie
 def store(request):
-    id_person = request.data.get('idPerson')
-    situation = request.data.get('situation')
-    mail = request.data.get('mailUser')
+    mails_array = request.data.get('mails')
 
-    mail_registered = Mails.objects.create(id_person, situation, mail)
-    mail_serialized = EmailSerializers(mail_registered)
-    return Response(mail_serialized.data)
+    for mail in mails_array:
+        try:
+            mail_registered = Mails(id_pessoa_cod_fk=mail['idPerson'],
+                                    situacao=mail['situation'], email=mail['userMail'])
+            mail_registered.save()
+            return Response({'Todos os dados de email foram salvos'})
+        except:
+            raise exceptions.APIException(
+                'Não foi possível salvar os dados de email')
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+@authentication_classes([SafeJWTAuthentication])
+@ensure_csrf_cookie
+def update(request):
+    mails_array = request.data.get('mails')
+
+    for mail in mails_array:
+        try:
+            userMail = Mails.objects.get(pk=mail['idMail'])
+            userMail.email = mail['userMail']
+            userMail.save()
+            return Response({'detail': 'Todos os dados de email foram atualizados'})
+        except:
+            raise exceptions.APIException(
+                'Não foi possível atualizar o registor de email')
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+@authentication_classes([SafeJWTAuthentication])
+@ensure_csrf_cookie
+def delete(request, id_mail):
+    try:
+        mail = Mails.objects.get(pk=id_mail)
+        mail.situacao = 0
+        mail.save()
+        return Response({'detail': 'Apagado com sucesso!'})
+    except:
+        raise exceptions.APIException('Não foi possível deletar o registro')
