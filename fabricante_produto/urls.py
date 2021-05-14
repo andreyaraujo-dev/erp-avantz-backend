1912af9f2ba78b297f894809326ec0826b8ad8c5
Andrey Araujo <jaab.andrey@gmail.com>
2021-05-14 12:30:47 -0300

    feat: implementation products groups

--- fabricante_produto/urls.py
+++ fabricante_produto/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_all, name='get_all'),
+    path('', views.get_all, name='get_all_manufacturer'),
+    path('create', views.create, name='create_manufacturer'),
+    path('update/<int:id>', views.update, name='update_manufacturer'),
+    path('delete/<int:id>', views.delete, name='delete_manufacturer'),
+    path('<int:id>', views.find_by_id, name='find_by_id_manufacturer'),
+    path('brand/<str:brand>', views.find_by_brand,
+         name='find_by_brand_manufacturer'),
 ]
