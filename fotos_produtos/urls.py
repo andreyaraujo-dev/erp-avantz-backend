31f9b8a7caf42ad95d646e99ffd5e61555304b0d
Andrey Araujo <jaab.andrey@gmail.com>
2021-05-31 17:27:07 -0300

    feat: imges products

--- fotos_produtos/urls.py
+++ fotos_produtos/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('upload/<int:product_id>', views.upload, name='upload_foto_produto'),
     path('<int:product_id>', views.find_by_product_id,
          name='find_photo_by_product_id'),
+    path('delete/<int:id>', views.delete, name='delete_photo'),
+    path('update/<int:id>', views.update, name='update_photo'),
+    path('', views.find_all, name='find_all_photos'),
 ]
